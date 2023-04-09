fad999803f7fca963154f63c09fe19374a6e7bf4
Pratiksha gite <118196235+Pratikshagite07@users.noreply.github.com>
2023-04-09 11:59:07 +0530

    Add files via upload

---
+++ eight.java
@@ -0,0 +1,14 @@
+ class eight {
+    public static void main(String[]args)
+    { 
+        for(int i=1;i<=5; i++){
+        
+          System.out.println("Hello"); 
+           break; 
+
+        }
+      
+      }
+    }
+    
+
